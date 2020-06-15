58288fc219558778b0b8c718485aab2bfddaea23
Saphal Patro <saphal1998@gmail.com>
2020-06-15 12:13:10 +0530

    removed singular phase events

--- types/Phases.ts
+++ types/Phases.ts
@@ -3,18 +3,6 @@ export enum DurationEventsPhase {
 	END = 'E',
 }
 
-export enum CompleteEventsPhase {
-	PHASE = 'X',
-}
-
-export enum InstantEventsPhase {
-	PHASE = 'I',
-}
-
-export enum CounterEventsPhase {
-	PHASE = 'C',
-}
-
 export enum AsyncEventsPhase {
 	NESTABLE_START = 'b',
 	NESTABLE_INSTANT = 'n',
@@ -32,31 +20,15 @@ export enum FlowEventsPhase {
 	END = 'f',
 }
 
-export enum SampleEventsPhase {
-	PHASE = 'P',
-}
-
 export enum ObjectEventsPhase {
 	CREATED = 'N',
 	SNAPSHOT = 'O',
 	DESTROYED = 'D',
 }
 
-export enum MetadataEventsPhase {
-	PHASE = 'M',
-}
-
 export enum MemoryDumpEventsPhase {
 	GLOBAL = 'V',
 	PROCESS = 'v',
-}
-
-export enum MarkEventsPhase {
-	PHASE = 'R',
-}
-
-export enum ClockSyncEventsPhase {
-	PHASE = 'c',
 }
 
 export enum ContextEventsPhase {
