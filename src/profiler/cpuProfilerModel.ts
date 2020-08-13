856ae799ffceb09996de11003933f5b474080b78
Saphal Patro <31125345+saphal1998@users.noreply.github.com>
2020-08-13 10:14:27 +0100

    Clarify Lighthouse CpuProfilerModel origins and comments

--- src/profiler/cpuProfilerModel.ts
+++ src/profiler/cpuProfilerModel.ts
@@ -2,7 +2,9 @@
  * @license Copyright 2020 The Lighthouse Authors. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- * This file is heavily derived from `https://github.com/GoogleChrome/lighthouse/blob/0422daa9b1b8528dd8436860b153134bd0f959f1/lighthouse-core/lib/tracehouse/cpu-profile-model.js`
+ *
+ * MODIFICATION NOTICE:
+ * This file is derived from `https://github.com/GoogleChrome/lighthouse/blob/0422daa9b1b8528dd8436860b153134bd0f959f1/lighthouse-core/lib/tracehouse/cpu-profile-model.js`
  * and has been modified by Saphal Patro (email: saphal1998@gmail.com)
  * The following changes have been made to the original file:
  * 1. Converted code to Typescript and defined necessary types
@@ -241,7 +243,7 @@ export class CpuProfilerModel {
    * by `createStartEndEvents()`.
    *
    * @param {HermesCPUProfile} profile
-   * @throws Profile must have atleast one sample
+   * @throws Profile must have at least one sample
    * @return {CPUProfileChunk}
    */
   static collectProfileEvents(profile: HermesCPUProfile): CPUProfileChunk {
